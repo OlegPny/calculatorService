36f161eafb780af67ea02c34c23adf121db4f11c
Oleg Primolennyy <Oleg.Primolennyy@x5.ru>
2020-12-13 14:44:45 +0700

    добавил api методов сложения и вычитания

---
+++ src/com/company/calculator/Calculator.java
@@ -0,0 +1,12 @@
+package com.company.calculator;
+
+public class Calculator {
+
+  public double addition(double first, double second) {
+    throw new ArithmeticException("Not implemented");
+  }
+
+  public double subtraction(double first, double second) {
+    throw new ArithmeticException("Not implemented");
+  }
+}
